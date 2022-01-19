d3f8b624292ce87f30e2bc96c5744ce87a18b0de
Alexandre Paillier <alexandre.paillier@ledger.fr>
2022-01-19 09:27:04 +0100

    Now loads the usb monitoring kernel module

--- nano-sniffer.py
+++ nano-sniffer.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import os
+import subprocess
 # Dependencies
 import pyshark
 import usb.core
@@ -12,10 +13,13 @@ if os.geteuid() != 0:
 
 dev = usb.core.find(idVendor=0x2c97)
 if dev is None:
-    print("Error: No Nano found !")
+    print('Error: No Nano found !')
     exit(1)
 
-print("Nano found on Bus %d, Device %d" % (dev.bus, dev.address))
+print('Nano found on Bus %d, Device %d' % (dev.bus, dev.address))
+
+# Load necessary kernel module (does nothing if already loaded)
+subprocess.call(['modprobe', 'usbmon'])
 
 capture = pyshark.LiveCapture(interface='usbmon%d' % (dev.bus),
                               display_filter='usb.device_address == %d && usb.capdata' % (dev.address))
